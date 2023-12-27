9fdc2ac87d09fae770944488ff0c81fc23211879
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-12-27 22:18:55 +0300

    simple logout

--- components/TopNav.jsx
+++ components/TopNav.jsx
@@ -5,7 +5,7 @@ import { useSession, signOut } from "next-auth/react";
 
 function TopNav() {
   const { data, status, loading } = useSession();
-  console.table({ data, status, loading });
+  // console.log({ data, status, loading });
 
   return (
     <nav className="nav shadow p-2 justify-content-between mb-3">
@@ -13,14 +13,28 @@ function TopNav() {
         🛒 NEXTECOM
       </Link>
 
-      <div className="d-flex">
-        <Link href="/login" className="nav-link">
-          Login
-        </Link>
-        <Link href="/register" className="nav-link">
-          Register
-        </Link>
-      </div>
+      {status === "authenticated" ? (
+        <>
+          <Link href="/dashboard/user" className="nav-link">
+            {data?.user?.name}
+          </Link>
+          <a
+            className="nav-link pointer"
+            onClick={() => signOut({ callbackUrl: "/login" })}
+          >
+            Logout
+          </a>
+        </>
+      ) : (
+        <div className="d-flex">
+          <Link href="/login" className="nav-link">
+            Login
+          </Link>
+          <Link href="/register" className="nav-link">
+            Register
+          </Link>
+        </div>
+      )}
     </nav>
   );
 }

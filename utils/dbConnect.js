e2af347d674860ec3cee1a89131dc16e9f97a209
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-12-30 20:16:57 +0300

    local to mongodb

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -5,7 +5,7 @@ const dbConnect = async () => {
     return;
   }
   // DB_URI_LOCAL for local connection
-  mongoose.connect(process.env.DB_URI_LOCAL, {
+  mongoose.connect(process.env.DB_URI, {
     dbName: "ryan-dhungel-next-ecom",
   });
 };

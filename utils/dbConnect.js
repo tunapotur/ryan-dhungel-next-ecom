d40a56f3f813f1758d0f715ca134f5dc197cadd0
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-12-30 20:23:55 +0300

    change db connection

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -11,21 +11,3 @@ const dbConnect = async () => {
 };
 
 export default dbConnect;
-
-/*
-// https://stackoverflow.com/questions/48917591/fail-to-connect-mongoose-to-atlas/48917626#48917626
-import mongoose from "mongoose";
-
-const connectMongoDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "SuccessTubitakDB",
-    });
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export default connectMongoDB;
-*/

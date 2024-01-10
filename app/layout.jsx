22aa4425cf2a581682911e2f26bac2e9d48711b8
Ahmet Tuna POTUR <tpotur@gmail.com>
2024-01-10 15:58:23 +0300

    Categories Context

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,16 +5,19 @@ import "bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
 import TopNav from "@/components/nav/TopNav";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
+import { CategoryProvider } from "@/context/category";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <SessionProvider>
-        <body>
-          <TopNav />
-          <Toaster />
-          {children}
-        </body>
+        <CategoryProvider>
+          <body>
+            <TopNav />
+            <Toaster />
+            {children}
+          </body>
+        </CategoryProvider>
       </SessionProvider>
     </html>
   );
